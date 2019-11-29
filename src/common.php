37ecbf93e6deaefe1c67838501764404f5a555fc
龙邦 <x135356@sina.com>
2019-11-30 06:12:54 +0800

    no msg

--- src/common.php
+++ src/common.php
@@ -1,10 +1,10 @@
 <?php
 
-class LswaggerPath extends \Lonban\Vcc\Classes\PathClass
+class LswaggerVccPath extends \Lonban\Vcc\Classes\PathClass
 {
     static $path = __DIR__;
 }
-class SwaggerApiPath extends \Lonban\Vcc\Classes\PathClass
+class SwaggerApiVccPath extends \Lonban\Vcc\Classes\PathClass
 {
     static $set_path = 'swagger-api\swagger-ui\dist';
 }
